bb66b4b2f3fb24aa059508184a0affbfc66efb6f
lnrxzzo <lorenzzo07sti@gmail.com>
2023-08-28 11:46:50 -0300

    att botoes com icones

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,26 +1,29 @@
 import React from "react";
 import './nav.css';
-import styled from 'styled-components';
+import Lapis from "../../icons/lapis.png"
+import Horario from "../../icons/relogio.png"
+import Livro from "../../icons/livro.png"
+import Licao from "../../icons/licao.png"
+import Usuario from "../../icons/usuario.png"
+
+
 
 
 function Nav() {
   return (
     <div>
-      <h1>Jornal senac</h1>
+      <h1>Área do aluno</h1>
       <div className="navbar">
-     
-        <button>Notas e Frequência</button>
-        <button>Horário</button>
-        <button>Biblioteca</button>
-        <button>Atividades</button>
-        <button>Meus Dados</button>
-        
 
-        {/* <StyledButton>Item 1 do menu</StyledButton>
-        <StyledButton>Item 2 do menu</StyledButton>
-        <StyledButton>Item 3 do menu</StyledButton>
-        <StyledButton>Item 4 do menu</StyledButton>
-        <StyledButton>Item 5 do menu</StyledButton> */}
+        <button><img src={Lapis}></img>Notas</button>
+        <button><img src={Horario}></img>Horário</button>
+        <button><img src={Livro} />Biblioteca</button>
+        <button><img src={Licao} />Atividades</button>
+        <button><img src={Usuario} />Meus Dados</button>
+
+
+
+
       </div>
     </div>
   )
@@ -29,12 +32,3 @@ function Nav() {
 export default Nav;
 
 
-const StyledButton = styled.button`
-background-color: #007bff;
-color: white;
-margin: 10px;
-padding: 10px 20px;
-border: none;
-border-radius: 4px;
-cursor: pointer;
-`;
